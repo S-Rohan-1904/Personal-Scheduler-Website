7783ecf9a163bf196057e769ff097a4701f2e288
S-Rohan-1904 <140544793+S-Rohan-1904@users.noreply.github.com>
2024-04-07 14:49:39 +0530

    Change Styles of Login Page

--- app/page.jsx
+++ app/page.jsx
@@ -25,10 +25,10 @@ export default function Login() {
       const response = await axios.post(
         "https://sutt-front-task2-d09a14a7c50b.herokuapp.com/auth/login",
         {
-          // email: email,
-          // password: password,
-          email: "f20230935@pilani.bits-pilani.ac.in",
-          password: "Roha0935",
+          email: email,
+          password: password,
+          // email: "f20230935@pilani.bits-pilani.ac.in",
+          // password: "Roha0935",
         }
       );
       setIsLoggedin(true);
@@ -60,25 +60,43 @@ export default function Login() {
 
   return (
     <main>
-      {isError && <h1>{errorMessage}</h1>}
-
-      <input
-        type="text"
-        name=""
-        id=""
-        placeholder="Email"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
-      />
-      <input
-        type="password"
-        name=""
-        id=""
-        placeholder="Password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <button onClick={handleSubmit}>Login</button>
+      <div className="flex items-center justify-center min-h-screen bg-gray-100">
+        <div className="px-10 py-10 mt-4 text-left bg-white shadow-lg">
+          <h3 className="text-2xl font-bold text-center">Login</h3>
+          <form>
+            {isError && <h1 className="text-red-500 my-5">{errorMessage}</h1>}
+            <div className="mt-4">
+              <div>
+                <label className="block" for="email">
+                  Email
+                </label>
+                <input
+                  type="text"
+                  placeholder="Email"
+                  class="w-full px-4 py-2 mt-2 border rounded-md focus:ring-blue-600"
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+              </div>
+              <div className="mt-4">
+                <label className="block">Password</label>
+                <input
+                  type="password"
+                  placeholder="Password"
+                  className="w-full px-4 py-2 mt-2 border rounded-md  focus:ring-blue-600"
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </div>
+              <button
+                className="px-6 py-2 mt-4 text-white bg-blue-600 rounded-lg hover:bg-blue-900"
+                onClick={handleSubmit}
+                type="submit"
+              >
+                Login
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
     </main>
   );
 }
